9e6bff6c3bfcc731b15f678d8768196312daa6fd
unknown <adam.jan.fila@gmail.com>
2019-09-14 14:40:03 +0200

    add routing

--- front-end/jlpt/src/app/app-routing.module.ts
+++ front-end/jlpt/src/app/app-routing.module.ts
@@ -1,8 +1,47 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProfilComponent} from "./profil/profil.component";
+import {LessonsComponent} from "./lessons/lessons.component";
+import {ResultsComponent} from "./results/results.component";
+import {ExerciseWizardComponent} from "./exercise-wizard/exercise-wizard.component";
+import {LessonWizardComponent} from "./lesson-wizard/lesson-wizard.component";
+import {TestWizardComponent} from "./test-wizard/test-wizard.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  //Konieczne będzie dodanie Guardsów - blokada przed wejściem na nie udostępnione dla konkretnego użytkownika dane
+  //User
+  /*{
+    path: '',
+    component: LoginPanelComponent
+  },*/
+  {
+    path: 'profil',
+    component: ProfilComponent
+  },
+  {
+    path: 'lessons',
+    component: LessonsComponent
+  },
+  {
+    path: 'results',
+    component: ResultsComponent
+  },
+
+  //Admin
+  {
+    path: 'exercise-wizard',
+    component: ExerciseWizardComponent
+  },
+  {
+    path: 'lesson-wizard',
+    component: LessonWizardComponent
+  },
+  {
+    path: 'test-wizard',
+    component: TestWizardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
