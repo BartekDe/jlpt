fbf6c5c2ab8504ba0451436b06f133c3463f3bb7
unknown <adam.jan.fila@gmail.com>
2019-09-14 14:31:03 +0200

    add standard components

--- front-end/jlpt/src/app/app.module.ts
+++ front-end/jlpt/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import bootstrap from "bootstrap";
+import { ProfilComponent } from './profil/profil.component';
+import { LessonsComponent } from './lessons/lessons.component';
+import { ResultsComponent } from './results/results.component';
+import { ExerciseWizardComponent } from './exercise-wizard/exercise-wizard.component';
+import { LessonWizardComponent } from './lesson-wizard/lesson-wizard.component';
+import { TestWizardComponent } from './test-wizard/test-wizard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfilComponent,
+    LessonsComponent,
+    ResultsComponent,
+    ExerciseWizardComponent,
+    LessonWizardComponent,
+    TestWizardComponent
   ],
   imports: [
     BrowserModule,
---
+++ front-end/jlpt/src/app/exercise-wizard/exercise-wizard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercise-wizard',
+  templateUrl: './exercise-wizard.component.html',
+  styleUrls: ['./exercise-wizard.component.css']
+})
+export class ExerciseWizardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
